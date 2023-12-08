e515fd5df4ac2f91b2fe40be9f310e95cafebc8c
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-08 16:59:24 -0300

    Add files via upload

---
+++ TapCode.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_page_config('Tap Code Translate', page_icon='👩‍💻')
+st.header('Tap Code Translate', divider= 'rainbow')
+st.subheader(':grey[by Hanna]')
+
+st.divider()
+
+st.title('Tradutor')
+st.write('Esse tradutor foi criado para aquelas pessoas que desejam traduzir ou decodificar alguma mensagem feita pela mesma.')
+st.write(':grey[*"O Tap Code, às vezes chamado de código de batida, é uma forma de codificar mensagens de texto letra por letra de uma forma muito simples. A mensagem é transmitida por meio de uma série de sons de toque, daí seu nome. O código de escuta tem sido comumente usado por prisioneiros para se comunicarem entre si. O método de comunicação geralmente é bater nas barras de metal, nos tubos ou nas paredes dentro de uma cela."*]')
+st.caption(':green[_Wikipedia_]')
+
+st.divider()
+
