1a94f539c14dc5b99a1b2fba74d7eb9544991903
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-09 03:13:51 -0300

    Update 0_Tradutor.py

--- pages/0_Tradutor.py
+++ pages/0_Tradutor.py
@@ -19,9 +19,10 @@ lista = ['. .','. ..','. ...','. ....','. .....','.. .','.. ..','.. ...','.. ...
 bra = [' ']
 
 for i in range(1,241):
-    codigo = st.text_area('Digite o código para o tradutor decodificar', key='str')
+    codigo = st.text_area('Digite o código para o tradutor decodificar', key=split)
     butao = st.button('Decodificar')   
     for cod1 in codigo:
+        split = abc.append(cod1)
         texto = texto + tradu
         if cod1 == " " + " ":
             tradu = abc[0]
